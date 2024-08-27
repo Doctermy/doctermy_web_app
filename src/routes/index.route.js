d5f2dfa3e97216d38fa47f0c141f088c9290ffa1
Chisom Sylvia <chisomsylvia95@gmail.com>
2024-08-27 05:24:09 +0100

    Revert "add hospital model"

--- src/routes/index.route.js
+++
@@ -1,9 +0,0 @@
-import Router from "express";
-const router = Router();
-import hospitalRouter from "./hospital.route.js"
-
-
-router.use("/api/v1/hospital", hospitalRouter);
-
-
-export default router;
